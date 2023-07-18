22cef62d2a6265388440ffe86d0dfea109cd2744
sanchitagarg1 <sanchitagarg2003@gmail.com>
2023-07-18 21:32:18 +0530

    Desktop-20 page1,2 done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+import PythonCourseComponent from "./components/PythonCourseComponent";
+import SelectPlanComponent from "./components/SelectPlanComponent";
+
+
+
+const Container = styled.div`
+  font-family: 'Poppins', sans-serif;
+  display:flex;
+  align-items:center;
+  flex-direction:column;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+
+      <PythonCourseComponent/>
+      <SelectPlanComponent/>
+
+    </Container>
   );
 }
 
